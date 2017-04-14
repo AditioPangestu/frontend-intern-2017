3b5d468646f07be90a52efbf9857a9e8e7be4bef
Aditio Pangestu <aditiopangestu@gmail.com>
2017-04-14 15:25:27 +0700

    Mendapatkan lovelist ke server

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var querystring = require('querystring');
 var http = require('http');
 var router = express.Router();
+var url = require('url');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -35,8 +36,7 @@ router.post('/', function (req, res) {
       });
       response.on('end', function() {
         var objectPrelo = JSON.parse(dataPrelo);
-        console.log(objectPrelo._data.token);
-          res.redirect('/lovelist?token='+objectPrelo._data.token);
+        res.redirect('/lovelist?token='+objectPrelo._data.token);
       })
     });
     httpreq.write(data);
@@ -45,7 +45,35 @@ router.post('/', function (req, res) {
 
 /* GET lovelsit page. */
 router.get('/lovelist', function(req, res, next) {
-  res.render('lovelist', { title: 'Lovelist' });
+    var url_parts = url.parse(req.url, true);
+    var query = url_parts.query;
+    var data = querystring.stringify({
+        'Authorization': ('Token '+ req.query.token)
+    });
+    var options = {
+    host: 'dev.prelo.id',
+    path: '/api/me/lovelist',
+    method: 'GET',
+    headers: {
+      'Authorization': ('Token '+ req.query.token)
+    }
+  };
+  var lovelistData="";
+  var lovelistObj;
+  var httpreq = http.request(options, function (response) {
+    response.setEncoding('utf8');
+    response.on('data', function (chunk) {
+      lovelistData+=chunk;
+    });
+    response.on('end', function() {
+      lovelistObj = JSON.parse(lovelistData);
+      console.log(lovelistObj);
+      lovelistObj.title = 'Lovelist';
+      res.render('lovelist', lovelistObj);
+    })
+  });
+  httpreq.write(data);
+  httpreq.end();
 });
 
 module.exports = router;
